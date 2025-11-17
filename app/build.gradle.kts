565c0ad9d264c6d6626d38ebd47283f2232b1a59
by074 <byang074@uottawa.ca>
2025-11-17 00:58:54 -0500

    Lab 8: new

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.productcatalognew"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.productcatalognew"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    implementation("com.google.firebase:firebase-database:20.3.0")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test:rules:1.5.0")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+}
---
+++ app/src/androidTest/java/com/example/productcatalognew/FirstTest.java
@@ -0,0 +1,56 @@
+package com.example.productcatalognew;
+
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+
+
+@RunWith(AndroidJUnit4.class)
+    @LargeTest
+    public class FirstTest {
+
+        private String mStringToBetyped;
+        private String mNumberToBetyped;
+        @Rule
+        public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+                MainActivity.class);
+
+        @Before
+        public void initValidString() {
+            // Specify a valid string.
+            mStringToBetyped = "Espresso";
+            mNumberToBetyped = "1234";
+        }
+
+        @Test
+        public void changeText_sameActivity() {
+            // Type text and then press the button.
+            onView(withId(R.id.editTextName))
+                    .perform(typeText(mStringToBetyped), closeSoftKeyboard());
+            onView(withId(R.id.editTextPrice))
+                    .perform(typeText(mNumberToBetyped), closeSoftKeyboard());
+
+            onView(withId(R.id.addButton)).perform(click());
+
+            // Check that the text was changed.
+            //onView(withId(R.id.listViewProducts))
+              //      .check(matches(withText(mStringToBetyped)));
+            onData(anything()).inAdapterView(withId(R.id.listViewProducts)).atPosition(0).onChildView(withId(R.id.textViewName)).check(matches(withText("Hello")));
+
+        }
+    }
